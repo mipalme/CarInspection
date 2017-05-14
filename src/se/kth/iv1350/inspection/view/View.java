2ae8b3bf6e86b77d558bbbc67e27d2f3aabf4956
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-14 17:12:13 +0200

    javadoc update

--- src/se/kth/iv1350/inspection/view/View.java
+++ src/se/kth/iv1350/inspection/view/View.java
@@ -2,36 +2,34 @@ package se.kth.iv1350.inspection.view;
 
 import se.kth.iv1350.inspection.controller.Controller;
 
-
+/**
+ * This class represents the outside world initiating the steps of the program
+ * (such as a customer paying for example).
+ * 
+ */
 public class View {
 	
 	private final Controller controller;
+        private final String currentVehicleToInspect = "ELF523";
 	
 	/**
-	 * 
+	 * Initiates an instance of the object controller.
 	 * @param controller. The object controller. 
 	 */
 	public View(Controller controller){
 		this.controller = controller;
 	}
-	
-	/**
-	 * This Vehicles are in queue outside the garage, only one can be inspected at the same time, you choose who!
-	 */
-	 private static String currentVehicleToInspect = "ELF523"; 
-                                                                                                    //"ELF523";  Registered vehicle
-	 											    //"LKF245"	 Registered vehicle
-	 											    //"LDK424"	 Registered vehicle
-	 											    //"NPC999"   Vehicle not in database
-	
+        /**
+         * Tests various system operations to make sure the program is functioning
+         * properly without logical errors.
+         */
 	public void testSystemOperations(){
 		//Different SystemOperations.
-		controller.newInspection(1);//number update display with
-		controller.registerCashPayment(1000, 1200); //what the inspection cost, what the constumer payed
-		controller.verifyVehicle(currentVehicleToInspect); //checks with database if this vehicle exists
-		controller.registerCardPayment(3141, "592 653 589", "Michael", 7932, 384, (double) 6267); //creditcard information
+		controller.newInspection(1);//number to update display with.
+		controller.registerCashPayment(1000, 1200); //what the inspection costs, what the constumer payed.
+		controller.verifyVehicle(currentVehicleToInspect); //checks with database if this vehicle exists.
+		controller.registerCardPayment(3141, "592 653 589", "Michael", 7932, 384, (double) 6267); //creditcard information.
 		controller.fetchNextInspectionAndStoreResults(currentVehicleToInspect); 
-		controller.printCurrentResult(currentVehicleToInspect);
-		
+		controller.printCurrentResult(currentVehicleToInspect);	
 	}
 }
