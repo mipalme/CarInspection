cdefb039c2f09bcf151b314fd87fb4c2726801e1
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-14 17:11:44 +0200

    javadoc update

--- src/se/kth/iv1350/inspection/integration/Printer.java
+++ src/se/kth/iv1350/inspection/integration/Printer.java
@@ -3,27 +3,24 @@ package se.kth.iv1350.inspection.integration;
 
 import se.kth.iv1350.inspection.model.Receipt;
 
-
-public class Printer {
-
-		
+/**
+ * This class handles the printer that prints both receipts and inspection results.
+ * 
+ */
+public class Printer {	
 	/**
-	 * 
-	 * @param receipt object that is to be printer when method is called.
+	 * Prints a given receipt.
+	 * @param receipt receipt that is to be printer when the method is called.
 	 */
-	
 	public void printReceipt(Receipt receipt){
-		//Prints the receipt
 		System.out.println("A receipt has been printed");
 	}
-	
 	/**
-	 * 
-	 * @param finalInspectionResult. The final inspectionResults that will be printed when method is called
-	 * @param registrationNumber. The registration number on the inspected vehicle
+	 * Prints the final results of the inspection.
+	 * @param finalInspectionResult. The inspectionResults that will be printed when the method is called.
+	 * @param registrationNumber. The registration number of the inspected vehicle.
 	 */
 	public void printResult(String finalInspectionResult, String registrationNumber){
-		//prints the inspectionresults
 		System.out.println("All results for the inspections :"+finalInspectionResult+" has been printed for Vehicle: " +registrationNumber+".");
 	}
 }
