e62048cd6e77c6e143381f0c568db29b027e677a
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-14 17:12:35 +0200

    javadoc update

--- src/se/kth/iv1350/inspection/startup/Seminar3.java
+++ src/se/kth/iv1350/inspection/startup/Seminar3.java
@@ -3,20 +3,26 @@ package se.kth.iv1350.inspection.startup;
 
 import se.kth.iv1350.inspection.controller.Controller;
 import se.kth.iv1350.inspection.view.View;
-
+/**
+ * This class contains the main method and is run upon startup.
+ * 
+ */
 public class Seminar3 {
 
-	
+	/**
+         * The main method that initiates the program sequence.
+         * @param args 
+         */
 	public static void main(String[] args) {
 	
 		/**
-		 * Creates a new instance of the controller object and view objects.
+		 * Creates a new instance of the controller object and view object.
 		 */
 		Controller controller = new Controller();
 		View view = new View(controller);
 		
 		/**
-		 * Different systemOperations that will be tested. 
+		 * Different system operations that will be tested. 
 		 */
 		view.testSystemOperations();
 	}
