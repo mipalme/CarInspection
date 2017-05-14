42a6500593343a4391847e29281b6926090836f6
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-14 17:12:05 +0200

    javadoc update

--- src/se/kth/iv1350/inspection/model/Vehicle.java
+++ src/se/kth/iv1350/inspection/model/Vehicle.java
@@ -1,15 +1,22 @@
 package se.kth.iv1350.inspection.model;
-
+/**
+ * This class handles the vehicle upon which the inspection is made.
+ * 
+ */
 public class Vehicle {
 	
-	/**
-	 * This class creates the vehicle object which holds a registration number.
-	 */
-	//.
-	private String registrationNumber;
+	private final String registrationNumber;
+        /**
+         * Creates a new instance of the type Vehicle which contains a registration number.
+         * @param registrationNumber The registration number of the vehicle.
+         */
 	public Vehicle(String registrationNumber){
 		this.registrationNumber = registrationNumber;
 	}
+        /**
+         * Finds out the registration number of the current instance of the vehicle.
+         * @return the registration number in question.
+         */
 	public String getRegistrationNumber(){
 		return this.registrationNumber;
 	}
