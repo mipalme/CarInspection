68533aa4df2de14939d7deb65362f60362a79bdb
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-14 17:11:27 +0200

    javadoc update

--- src/se/kth/iv1350/inspection/integration/Garage.java
+++ src/se/kth/iv1350/inspection/integration/Garage.java
@@ -1,23 +1,28 @@
 
 package se.kth.iv1350.inspection.integration;
-
+/**
+ * This class handles the garage where the inspection takes place,
+ * the garage also contains the display which shows what customer should be served.
+ * 
+ */
 public class Garage {
-	
+	/**
+         * Opens the garage door.
+         */
 	public void openDoor(){
-		//close the garage door
 		System.out.println("Door has been opened");
 	}
-	
+	/**
+         * Closes the garage door.
+         */
 	public void closeDoor(){
-		//opens the garage door
 		System.out.println("Door has been closed");
 	}
 	/**
-	 * 
+	 * Updates the display with a new number.
 	 * @param number. The number that the display will show.
 	 */
 	public void updateDisplay(int number){
-		//updates the display
 		System.out.println("Display has been updated with the number: "+number);
 	}
 }
