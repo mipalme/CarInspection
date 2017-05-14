b439479a3391057a807f3b7d92083b6da336a88d
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-14 17:11:11 +0200

    javadoc update

--- src/se/kth/iv1350/inspection/integration/CashRegister.java
+++ src/se/kth/iv1350/inspection/integration/CashRegister.java
@@ -1,22 +1,32 @@
 
 package se.kth.iv1350.inspection.integration;
-
+/**
+ * This class handles the cash register which is used if the customer pays with cash.
+ * 
+ */
 public class CashRegister {
-	
+	/**
+         * The amount of money in the cash register.
+         */
 	public double cashInRegister = 500;
-        
-        //The cash register has 500 dollars in it to begin with.
-	
+        /**
+         * Adds a value to the current amount of money in the register.
+         * @param cost The value to be added.
+         */
 	public void updateCash(double cost){
 		cashInRegister = cost + cashInRegister;
 		System.out.println("The cashregister now contains "+cashInRegister+" $");
 	}
+        /**
+         * Calculated how much change the customer should receive.
+         * @param cost The cost of the purchase.
+         * @param amountPayed The amount of money payed by the customer.
+         * @return The change the customer shall receive.
+         */
 	public double calculateChange(double cost, double amountPayed){
 		updateCash(cost);
 		double change = amountPayed - cost;
 		System.out.println("Your change is: " +change+" $");
 		return change;
-	}
-	
-	
+	}	
 }
