75fe5aac167393c936446bcb9d14530eb93dc78c
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-14 17:11:21 +0200

    javadoc update

--- src/se/kth/iv1350/inspection/integration/DatabaseManager.java
+++ src/se/kth/iv1350/inspection/integration/DatabaseManager.java
@@ -3,42 +3,45 @@ package se.kth.iv1350.inspection.integration;
 
 import se.kth.iv1350.inspection.data.Database;
 import se.kth.iv1350.inspection.model.Vehicle;
-
-public class DatabaseManager {
-	
-	//This Class is responsible for database calls, Database can only be called from here.
-	
+/**
+ * This class serves as the middleman between database and controller, it is the only class
+ * that calls from the database.
+ *
+ */
+public class DatabaseManager {	
 	/**
-	 * new instance of the database class.
+	 * New instance of the database class.
 	 */
 	Database database = new Database();
 
 	/**
-	 * 
-	 * @param vehicle. Vehicle with a specific registrationNumber is entered to find the corresponding inspections in the database.
-	 * @return If there is a inspection for the given vehicle given in (@param) this method will return the cost for the full inspection.
+	 * Looks if there are any inspections associated with the vehicle, 
+         * and also checks the cost.
+	 * @param vehicle. Vehicle which holds the registration number connected to the inspections.
+	 * @return The cost of the full inspection (if there is an inspection to be found).
 	 */
 	public double findInspection(Vehicle vehicle){
 		return database.fetchInspection(vehicle);
 	}
 	/**
-	 * 
-	 * @param vehicle. Vehicle with a specific registrationNumber is entered to find the corresponding inspectionCheckList in the database
-	 * @return If there is a inspectionChecklist for the given vehicle the next inspection on that checklist will be returned.
+	 * Goes through the inspections associated to the specified vehicle, while returning the next
+         * inspection in the list.
+	 * @param vehicle. The vehicle under inspection.
+	 * @return If there is an inspectionChecklist for the given vehicle the next inspection on that checklist will be returned.
 	 */
 	public String findInspectionChecklist(Vehicle vehicle){
 		return database.fetchInspectionChecklist(vehicle);
 	}
 	/**
-	 * 
-	 * @param the current inspection on the inspectionChecklist.
+	 * Stores the current result together with the previous results in a new updated result.
+	 * @param currentCompletedInspection The current inspection on the inspectionChecklist.
 	 */
 	public void storeCurrentResult(String currentCompletedInspection){
 		database.saveCurrentResult(currentCompletedInspection);
 	}
 	/**
-	 * 
-	 * @param vehicle. Vehicle with a specific registrationNumber is entered to find the corresponding final inspection result
+	 * Finds the final result of the specified vehicle in the form of one long string.
+	 * @param vehicle. The vehicle associated with the result you want access to.
 	 * @return Returns the corresponding final results for the given vehicle. 
 	 */
 	public String findFinalResult(Vehicle vehicle){
