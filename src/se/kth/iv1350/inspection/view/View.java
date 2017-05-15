39d70bce78d0012b109f93edbcfa17ae008d08c5
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-15 15:41:28 +0200

    seminar 4 changes

--- src/se/kth/iv1350/inspection/view/View.java
+++ src/se/kth/iv1350/inspection/view/View.java
@@ -1,6 +1,9 @@
 package se.kth.iv1350.inspection.view;
 
 import se.kth.iv1350.inspection.controller.Controller;
+import se.kth.iv1350.inspection.data.InvalidVehicleException;
+import se.kth.iv1350.inspection.util.LogHandler;
+import java.io.IOException;
 
 /**
  * This class represents the outside world initiating the steps of the program
@@ -10,26 +13,42 @@ import se.kth.iv1350.inspection.controller.Controller;
 public class View {
 	
 	private final Controller controller;
-        private final String currentVehicleToInspect = "ELF523";
-	
+        private final String currentVehicleToInspect = "ELF524";
+        LogHandler logger;
+       
 	/**
-	 * Initiates an instance of the object controller.
-	 * @param controller. The object controller. 
+	 * Initiates an instance of the object controller, also checks for IOException,
+         * and sets the logger to null if an exception occurred.
+	 * @param controller. The object controller.
+         * 
 	 */
 	public View(Controller controller){
-		this.controller = controller;
+		this.controller = controller; 
+                try{
+                     logger = new LogHandler();
+                }
+                catch (IOException e){
+                    logger = null;
+                }    
 	}
         /**
          * Tests various system operations to make sure the program is functioning
          * properly without logical errors.
          */
-	public void testSystemOperations(){
+	public void testSystemOperations (){
 		//Different SystemOperations.
-		controller.newInspection(1);//number to update display with.
-		controller.registerCashPayment(1000, 1200); //what the inspection costs, what the constumer payed.
-		controller.verifyVehicle(currentVehicleToInspect); //checks with database if this vehicle exists.
-		controller.registerCardPayment(3141, "592 653 589", "Michael", 7932, 384, (double) 6267); //creditcard information.
-		controller.fetchNextInspectionAndStoreResults(currentVehicleToInspect); 
-		controller.printCurrentResult(currentVehicleToInspect);	
+                try{
+                    controller.newInspection(1);//number to update display with.
+                    controller.registerCashPayment(1000, 1200); //what the inspection costs, what the constumer payed.
+                    System.out.println("Inspections found! The cost of the inspections are: "+controller.verifyVehicle(currentVehicleToInspect)+"$"); //checks with database if this vehicle exists.
+                    controller.registerCardPayment(3141, "592 653 589", "Michael", 7932, 384, (double) 6267); //creditcard information.
+                    System.out.println("Your inspections are: "+controller.findInspectionChecklist(currentVehicleToInspect));
+                    controller.fetchNextInspectionAndStoreResults(currentVehicleToInspect); 
+                    controller.printCurrentResult(currentVehicleToInspect);	
+                }
+                catch(InvalidVehicleException invalidVehicle){
+                    System.out.println(invalidVehicle.getMessage()); 
+                    logger.logException(invalidVehicle);
+                }
 	}
 }
