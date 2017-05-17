9dbf720dca9266232f2cfc0f4e14dfaa785c81ae
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-17 14:33:40 +0200

    seminar 4 changes

--- src/se/kth/iv1350/inspection/model/Vehicle.java
+++ src/se/kth/iv1350/inspection/model/Vehicle.java
@@ -7,11 +7,31 @@ public class Vehicle {
 	
 	private final String registrationNumber;
         /**
-         * Creates a new instance of the type Vehicle which contains a registration number.
+         * Instance variable describing the vehicles driving type.
+         */
+        public Strategy driveType;
+        /**
+         * Checks the vehicles driving type.
+         * @return The vehicles driving type. 
+         */
+        public String getDriveType(){
+            return driveType.driveType();
+        }
+        /**
+         * Changes a vehicles driving type.
+         * @param newDriveType The new driving type.
+         */
+        public void setDriveType(Strategy newDriveType){
+            driveType = newDriveType;
+        }
+        /**
+         * Creates a new instance of the type Vehicle which contains a registration number,
+         * all vehicles have all-wheel-drive by default.
          * @param registrationNumber The registration number of the vehicle.
          */
 	public Vehicle(String registrationNumber){
 		this.registrationNumber = registrationNumber;
+                driveType = new AllWheelDrive();
 	}
         /**
          * Finds out the registration number of the current instance of the vehicle.
