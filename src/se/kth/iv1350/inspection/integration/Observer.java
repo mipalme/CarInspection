940ea49a24ae59d8eac3639c1366a565bcb26bea
Michael <Michael@n170-p55.kthopen.kth.se>
2017-05-17 13:02:20 +0200

    seminar 4 changes

--- src/se/kth/iv1350/inspection/integration/Observer.java
+++ src/se/kth/iv1350/inspection/integration/Observer.java
@@ -6,6 +6,10 @@ package se.kth.iv1350.inspection.integration;
  * 
  */
 public interface Observer {
-   
-    public void CountPassOrFail (String result); 
+    /**
+     * Blueprint for a method to be used that counts the passes and fails
+     * of several inspections.
+     * @param result The result of a single inspection (pass or fail).
+     */
+    public void countPassOrFail (String result); 
 }
